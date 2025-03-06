2bf9a15a3567576f89f2a42e7880d393bfeca98d
shuoli90 <lishuo1@seas.upenn.edu>
2025-03-06 13:27:39 -0500

    update training

--- efficient_reasoning/sfttrainers.py
+++ efficient_reasoning/sfttrainers.py
@@ -1,9 +1,9 @@
 from trl import SFTTrainer
 import torch
 import torch.nn.functional as F
-from transformers import DataCollatorWithPadding
 from torch.utils.data import DataLoader
-
+from efficient_reasoning.utils import evaluate, Benchmark
+from tqdm import tqdm
 
 def selective_log_softmax(logits, index):
     """
@@ -40,6 +40,12 @@ def selective_log_softmax(logits, index):
     return per_token_logps
 
 
+def _get_per_token_logps(model, input_ids, attention_mask, labels):
+    # We add 1 to `logits_to_keep` because the last logits of the sequence is later excluded
+    logits = model(input_ids=input_ids, attention_mask=attention_mask).logits[:, :-1, :]
+    return selective_log_softmax(logits, labels)
+
+
 class ASFTTrainer(SFTTrainer):
     def get_train_dataloader(self):
         """Ensure `advantages` is retained in the dataloader"""
@@ -47,54 +53,7 @@ class ASFTTrainer(SFTTrainer):
             self.train_dataset,
             batch_size=self.args.train_batch_size,
             collate_fn=self.data_collator,
-            shuffle=True,
-        )
-
-    def get_eval_dataloader(self, eval_dataset=None):
-        """Ensure `advantages` is retained in the dataloader"""
-        eval_dataset = eval_dataset if eval_dataset is not None else self.eval_dataset
-        return DataLoader(
-            eval_dataset,
-            batch_size=self.args.eval_batch_size,
-            collate_fn=self.data_collator,
-        )
-        
-    # def _get_per_token_logps(self, model, input_ids, attention_mask, prediction_ids, logits_to_keep):
-        # We add 1 to `logits_to_keep` because the last logits of the sequence is later excluded
-        # logits = model(input_ids=input_ids, attention_mask=attention_mask, logits_to_keep=logits_to_keep).logits
-        # return selective_log_softmax(logits, prediction_ids)
-    
-    def _get_per_token_logps(self, model, input_ids, attention_mask, logits_to_keep):
-        # We add 1 to `logits_to_keep` because the last logits of the sequence is later excluded
-        logits = model(input_ids=input_ids, attention_mask=attention_mask, logits_to_keep=logits_to_keep + 1).logits
-        logits = logits[:, :-1, :]  # (B, L-1, V), exclude the last logit: it corresponds to the next token pred
-
-        input_ids = input_ids[:, -logits_to_keep:]
-        # For transformers<=4.48, logits_to_keep argument isn't supported, so here we drop logits ourselves.
-        # See https://github.com/huggingface/trl/issues/2770
-        logits = logits[:, -logits_to_keep:]
-        return selective_log_softmax(logits, input_ids)
-
-    # def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
-    #     prompt_ids, attention_mask, = inputs['input_ids'], inputs['attention_mask']
-    #     completion_ids = inputs['labels']
-    #     # rotate the attention mask by 1
-    #     completion_mask = torch.roll(attention_mask, shifts=1, dims=1)
-    #     advantages = inputs['advantages']
-    #     logits_to_keep = completion_ids.size(1)
-    #     per_token_logps = self._get_per_token_logps(model, prompt_ids, attention_mask, completion_ids, logits_to_keep)
-    #     loss = -((per_token_logps * advantages) * completion_mask).sum() / completion_mask.sum()
-    #     return loss
-
-class QSFTTrainer(ASFTTrainer):
-    
-    def get_train_dataloader(self):
-        """Ensure `advantages` is retained in the dataloader"""
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.args.train_batch_size,
-            collate_fn=self.data_collator,
-            shuffle=True,
+            shuffle=False,
         )
 
     def get_eval_dataloader(self, eval_dataset=None):
@@ -107,24 +66,67 @@ class QSFTTrainer(ASFTTrainer):
         )
     
     def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
-        prompt_ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
-        completion_ids, completion_mask = inputs['completion_ids'], inputs['completion_mask']
-        input_ids = torch.cat([prompt_ids, completion_ids], dim=1)
-        attention_mask = torch.cat([attention_mask, completion_mask], dim=1)
-        logits_to_keep = completion_ids.size(1)
-        advantages = 1 - inputs['q_value']
-        per_token_logps = self._get_per_token_logps(model, input_ids, attention_mask, logits_to_keep)
-        loss = -((per_token_logps * advantages) * completion_mask).sum() / completion_mask.sum()
+        input_ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
+        labels, completion_mask = inputs['labels'][:, 1:], inputs['completion_mask'][:, 1:]
+        # labels = F.pad(labels, (0, 1), value=self.processing_class.pad_token_id)
+        # shift_labels = labels[..., 1:].contiguous()
+        labels = labels[:, 1:]
+        advantages = inputs['advantages'][:, 1:]
+        per_token_logps = _get_per_token_logps(model, input_ids, attention_mask, labels) * completion_mask
+        loss = -(per_token_logps * advantages).sum() / completion_mask.sum()
         return loss
+    
+    def evaluate(
+        self,
+        ignore_keys=None,
+        metric_key_prefix="eval",
+    ):
+        correct = 0
+        self.model.eval()
+        with torch.no_grad():
+            for i in tqdm(range(0, len(self.eval_dataset), self.args.eval_batch_size), total=len(self.eval_dataset) // self.args.eval_batch_size):
+                batch = self.eval_dataset[i:i+self.args.eval_batch_size]
+                # problems = batch['problem']
+                # targets = batch['answer']
+                problems = batch['problem']
+                targets = [item['answer'] for item in problems]
+                problems = [item['problem'] for item in problems]
+                outputs = self.processing_class(problems, return_tensors='pt', padding=True, padding_side='left')
+                outputs = self.model.generate(
+                    input_ids=outputs['input_ids'].to(self.model.device),
+                    attention_mask=outputs['attention_mask'].to(self.model.device),
+                    temperature=0.7,
+                    top_p=0.95,
+                    max_new_tokens=512,
+                )
+                responses = [self.processing_class.decode(output, skip_special_tokens=True) for output in outputs]
+                rewards = evaluate('MATH-500', responses, targets)
+                correct += sum(rewards)
+        # log the accuracy
+        accuracy = correct / len(self.eval_dataset)
+        metrics = {f"{metric_key_prefix}_acc": accuracy}
+        self.log(metrics)
+        self.model.train()
+        return accuracy
+    
+
+class QSFTTrainer(ASFTTrainer):
+    def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
+        input_ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
+        labels, completion_mask = inputs['labels'][:, 1:], inputs['completion_mask'][:, 1:]
+        advantages = 1-inputs['advantages'][:, 1:]
+        per_token_logps = _get_per_token_logps(model, input_ids, attention_mask, labels) * completion_mask
+        loss = -(per_token_logps * advantages).sum() / completion_mask.sum()
+        return loss
+
 
 class NSFTTrainer(ASFTTrainer):
     def compute_loss(self, model, inputs, return_outputs=False, num_items_in_batch=None):
-        prompt_ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
-        completion_ids, completion_mask = inputs['completion_ids'], inputs['completion_mask']
-        input_ids = torch.cat([prompt_ids, completion_ids], dim=1)
-        attention_mask = torch.cat([attention_mask, completion_mask], dim=1)
-        logits_to_keep = completion_ids.size(1)
-        per_token_logps = self._get_per_token_logps(model, input_ids, attention_mask, logits_to_keep)
-        loss = -(per_token_logps * completion_mask).sum() / completion_mask.sum()
-        breakpoint()
+        input_ids, attention_mask = inputs['input_ids'], inputs['attention_mask']
+        labels, completion_mask = inputs['labels'][:, 1:], inputs['completion_mask'][:, 1:]
+        # labels = F.pad(labels, (0, 1), value=self.processing_class.pad_token_id)
+        # shift_labels = labels[..., 1:].contiguous()
+        labels = labels[:, 1:]
+        per_token_logps = _get_per_token_logps(model, input_ids, attention_mask, labels) * completion_mask
+        loss = -(per_token_logps).sum() / completion_mask.sum()
         return loss

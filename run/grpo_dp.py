7ec7b4a12da99b04581a888acac8ddd773ef8712
shuoli90 <lishuo1@ivy.seas.upenn.edu>
2025-04-25 22:30:02 -0400

    change reward function

--- run/grpo_dp.py
+++ run/grpo_dp.py
@@ -8,6 +8,7 @@ from efficient_reasoning.grpo_trainer import GRPOTrainer
 from efficient_reasoning.grpo_config import GRPOConfig
 import wandb
 import torch.distributed as dist
+from efficient_reasoning.utils import evaluate
 
 
 @dataclass
@@ -29,51 +30,8 @@ class GRPOScriptArguments(ScriptArguments):
         }
     )
 
-def accuracy_reward(completions, solution, **kwargs):
-    """Reward function that checks if the completion is the same as the ground truth."""
-    # contents = [completion[0]["content"] for completion in completions]
-    contents = completions
-    rewards = []
-    for content, sol in zip(contents, solution):
-        gold_parsed = parse(sol, extraction_mode="first_match", extraction_config=[
-            LatexExtractionConfig(), 
-            ExprExtractionConfig(), 
-            StringExtractionConfig()])
-        if len(gold_parsed) != 0:
-            # We require the answer to be provided in correct latex (no malformed operators)
-            answer_parsed = parse(
-                content,
-                extraction_config=[
-                    LatexExtractionConfig(
-                        normalization_config=NormalizationConfig(
-                            nits=False,
-                            malformed_operators=False,
-                            basic_latex=True,
-                            equations=True,
-                            boxed=True,
-                            units=True,
-                        ),
-                        # Ensures that boxed is tried first
-                        boxed_match_priority=0,
-                        try_extract_without_anchor=False,
-                    )
-                ],
-                extraction_mode="first_match",
-            )
-            # Reward 1 if the content is the same as the ground truth, 0 otherwise
-            try:
-                reward = float(verify(answer_parsed, gold_parsed))
-            except Exception as e:
-                print(e)
-                reward = 0.0
-        else:
-            # If the gold solution is not parseable, we reward 1 to skip this example
-            reward = 1.0
-            print("Failed to parse gold solution: ", sol)
-        rewards.append(reward)
-
-    return rewards
-
+def accuracy_reward(prompts, completions, answer):
+    return evaluate('MATH-500', completions, answer)
 
 def format_reward(completions, **kwargs):
     """Reward function that checks if the completion has a specific format."""

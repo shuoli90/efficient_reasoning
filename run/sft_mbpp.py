b9ecf3b7b1ed6536061e503dd36ce9d2ec472c6a
Sagnik Anupam <sagnikanupam@gmail.com>
2025-05-16 18:36:00 -0400

    sft for mbpp

---
+++ run/sft_mbpp.py
@@ -0,0 +1,79 @@
+from trl import SFTConfig, SFTTrainer, DataCollatorForCompletionOnlyLM
+from datasets import Dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from peft import LoraConfig
+from datasets import load_dataset
+    
+
+if __name__ == "__main__":
+    # model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-1.5B', device_map="auto")
+    # tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-1.5B')
+    # data_path = '/home/leoh/efficient_reasoning/data/MATH-500/7BDistillCorrectOnly.jsonl'
+    
+    model = AutoModelForCausalLM.from_pretrained('Qwen/Qwen2.5-0.5B', device_map="auto")
+    tokenizer = AutoTokenizer.from_pretrained('Qwen/Qwen2.5-0.5B')
+    data_path = "../data/MBPPPlus/train.jsonl"
+    
+    # data_path = '/home/leoh/efficient_reasoning/data/MATH-500/train.jsonl'
+    data = []
+    with open(data_path) as f:
+        for line in f:
+            data.append(eval(line))
+
+    train_dataset = Dataset.from_list(data)
+
+    def formatting_prompts_func(examples):
+        output_text = []
+        for index in range(len(examples['prompt'])):
+            # output_text.append(f"Problem:\n{examples['problem'][index]}\n\nSolution:\n{examples['solution'][index]}")
+            #output_text.append(f"### Question: {examples['problem'][index]}\n### Answer: {examples['solution'][index]}")
+            description = examples["prompt"][index]
+            test_example = examples["test_list"][index][0]
+            prompt = f'"""\n{description}\n{test_example}\n"""\n'
+            output_text.append(prompt)
+        return output_text
+    
+    # instruction = 'Problem:\n'
+    # response = 'Solution:\n'
+    instruction = '### Question:'
+    response = '### Answer:'
+    collator = DataCollatorForCompletionOnlyLM(
+        instruction_template=instruction,
+        response_template=response,
+        tokenizer=tokenizer,
+    )
+    
+    sft_config = SFTConfig(
+        # learning_rate=1e-5,
+        # lr_scheduler_type='cosine',
+        num_train_epochs=3, 
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=2,
+        output_dir=f'../scripts/sft_0.5B',
+        logging_steps=1,
+        save_strategy="steps",
+        save_steps=500,
+        gradient_accumulation_steps=2,
+        # max_length=2048,
+        )
+    
+    # peft_config = LoraConfig(
+    #     r=16,
+    #     lora_alpha=32,
+    #     lora_dropout=0.05,
+    #     target_modules="all-linear",
+    #     modules_to_save=["lm_head", "embed_token"],
+    #     task_type="CAUSAL_LM",
+    #     peft_type="LORA", #PEFT method type
+    # )
+    
+    trainer = SFTTrainer(
+        model=model,
+        train_dataset=train_dataset,
+        # peft_config=peft_config,
+        args=sft_config,
+        data_collator=collator,
+        formatting_func=formatting_prompts_func,
+    )
+    
+    trainer.train()
